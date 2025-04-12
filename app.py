2400403705c57b6db9a23c83dfc3d89a1c0df09a
ballaprr <ballaprr@oregonstate.edu>
2025-04-11 21:01:23 -0700

    sqlalechmy database

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-import sqlite3
 import os
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///processor.db'
+db = SQLAlchemy(app)
 
-conn = sqlite3.connect('database.db')
 # Database:
-# Receipt table: id (char(200)), relatiler (char(100)), purchaseDate (Date), purchaseTime(Time), total(float)
+# Receipt table: id (char(200)), retailer (char(100)), purchaseDate (Date), purchaseTime(Time), total(float)
 # Item table: id (int), shortDescription (char(200)), price(float)
+class Receipt(db.Model):
+    id = db.Column(db.String(length=50), primary_key=True)
+    retailer = db.Column(db.String(length=200), nullable=False)
+    purchaseDate = db.Column(db.DateTime(), nullable=False)
+    purchaseTime = db.Column(db.Time(), nullable=False)
+    total = db.Column(db.Float(), nullable=False)
 
+class Item(db.Model):
+    receipt_id = db.Column(db.String(50), db.ForeignKey('recepit.id'), nullable=False)
+    description = db.Column(db.String(length=200), nullable=False)
+    price = db.Column(db.Float(), nullable=False, unique=False)
 
 # post request: pass in receipt json and store in recept and item table, generate id and store, response should be the id
 
