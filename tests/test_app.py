f9939da0b4078a9a87e56372c297c1628e7f9864
ballaprr <ballaprr@oregonstate.edu>
2025-04-14 01:08:00 -0700

    added test case

--- tests/test_app.py
+++ tests/test_app.py
@@ -35,3 +35,39 @@ def test_receiptprocess():
         assert get_response.status_code == 200
         json_response = get_response.get_json()
         assert json_response['points'] == 109
+
+def test_receiptprocess1():
+    with app.test_client() as c:
+        response = c.post('/receipts/process', json={
+        "retailer": "Target",
+        "purchaseDate": "2022-01-01",
+        "purchaseTime": "13:01",
+        "items": [
+            {
+            "shortDescription": "Mountain Dew 12PK",
+            "price": "6.49"
+            },{
+            "shortDescription": "Emils Cheese Pizza",
+            "price": "12.25"
+            },{
+            "shortDescription": "Knorr Creamy Chicken",
+            "price": "1.26"
+            },{
+            "shortDescription": "Doritos Nacho Cheese",
+            "price": "3.35"
+            },{
+            "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+            "price": "12.00"
+            }
+        ],
+        "total": "35.35"
+        })
+        assert response.status_code == 201
+        json_response = response.get_json()
+        assert 'id' in json_response
+        assert isinstance(json_response['id'], str)
+
+        get_response = c.get(f"/receipts/{json_response['id']}/points")
+        assert get_response.status_code == 200
+        json_response = get_response.get_json()
+        assert json_response['points'] == 28
